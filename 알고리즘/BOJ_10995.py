83c0c001662de3aa248b73c78df439b392942c1d
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-04-13 17:42:03 +0900

    Update BOJ_10995.py

--- 알고리즘/BOJ_10995.py
+++ 알고리즘/BOJ_10995.py
@@ -5,3 +5,17 @@ for i in range(N):
     for j in range(N):#매개변수 만큼 *을 출력하나 끝에 띄움
         print("*",end=" ")
     print()
+#방법2
+n=int(input())
+for i in range(n):
+    if i %2==0:
+        print("* "*n)
+    else:
+        print(" *"*n)
+#방법3
+n = int(input())
+line = "* " * n
+
+for i in range(n):
+    print(line)
+    line = line[::-1]
