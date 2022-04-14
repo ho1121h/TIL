b4f073cea0884ea19715bb40afea2131e53597a1
ho1121h <102492367+ho1121h@users.noreply.github.com>
2022-04-14 21:55:01 +0900

    Update 2일차.py

--- 알고리즘/2일차.py
+++ 알고리즘/2일차.py
@@ -11,3 +11,46 @@ for i in range(n):#0부터 반복할 범위 설정
 n_2 = sorted(n_1)#리스트를 새로 정렬
 for i in range(len(n_1)):#리스트 크기만큼 반복
     print(n_2[i])#리스트를 인덱싱하여 하나 씩 추가
+
+# List Comprehension http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=4348&sca=pyc0
+number=[i**2 for i in range(1,int(input())+1)]
+print(number)
+
+# List Comprehension http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=4356&sca=pyc0
+n = int(input())
+numbers = [f"No.{i}" for i in range(1, n + 1)]
+print(numbers)
+
+# 9줄 입력 ,최댓값, 최댓값 자릿수
+number = [int(input()) for i in range(9) ]
+print(max(number))
+print(number.index(max(number))+1)
+
+# http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=4353&sca=pyc0
+#frist 입력
+line=[list(map(int,input().split())) for i in range(2)]
+
+#second 입력
+line2=[list(map(int,input().split())) for j in range(2)]
+
+for i in range(2):
+    for j in range(3):
+        print(line[i][j] * line2[i][j], end=" ")
+    print()
+
+'''
+3 6 9
+8 5 2
+9 8 7
+6 5 4
+'''
+list_a = [list(map(int, input().split())) for i in range(2)]
+list_b = [list(map(int, input().split())) for i in range(2)]
+list_c = [[0] * 3 for _ in range(2)]
+for i in range(2):
+    for j in range(3):
+        list_c[i][j] = list_a[i][j] * list_b[i][j]
+for i in range(2):
+    for j in range(3):
+        print(list_c[i][j], end=" ")
+    print()
